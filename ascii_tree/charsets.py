5d2746c00a4b6cddf03211d7bf707212f10e3106
Spandan Bemby <spandan.777@gmail.com>
2019-09-23 15:49:49 -0700

    add support for ascii and unicode

---
+++ ascii_tree/charsets.py
@@ -0,0 +1,57 @@
+'''
+parametrize character sets
+Each char set can have: box top-char, side char, corner chars
+possibly connection points etc.
+Should have a sensible way of picking chars since not
+all chars sets will need to define all types separately.
+'''
+
+class Charset:
+    def __init__(self, xside=None, yside=None, top=None, bottom=None, left=None, right=None,
+                 top_left=None, top_right=None, bottom_left=None, bottom_right=None, top_out=None,
+                 bottom_out=None, left_out=None, right_out=None):
+        '''
+        Accepts characters for different elements of the box.
+        Minimally, (xside, yside) or (top, left, bottom, right)
+        must be specified. The rest are intended to allow  finer control.
+
+        xside: character to use for top and bottom, i.e. along x-axis
+        yside: character to use for left and right, i.e. along y-axis
+        top: character to use for box top. defaults to xside
+        bottom:
+        left: character to use for box left. defaults to yside
+        right:
+        top_left: character to use for top-left corner. defaults to xside
+        top_right:
+        bottom_left:
+        bottom_right:
+        left_out: character where an edge connects on the left. defaults to yside
+        right_out:
+        top_out: character where an edge connects on the left. defaults to xside
+        bottom_out:
+        '''
+        if (xside is None or yside is None) and \
+           (left is None or right is None or top is None or bottom is None):
+            raise ValueError('Underdefined character set')
+
+        self.xside = xside or top
+        self.yside = yside or left
+        self.top = top or xside
+        self.bottom = bottom or xside
+        self.left = left or yside
+        self.right = right or yside
+
+        self.top_left = top_left or xside
+        self.top_right = top_right or xside
+        self.bottom_left = bottom_left or xside
+        self.bottom_right = bottom_right or xside
+        self.top_out = top_out or xside
+        self.bottom_out = bottom_out or xside
+        self.left_out = left_out or yside
+        self.right_out = right_out or yside
+
+
+
+Ascii = Charset(xside='-', yside='|')
+Unicode = Charset(xside='─', yside='│', top_left='┌', top_right='┐', bottom_left='└', bottom_right='┘',
+                  left_out='┤', right_out='├', top_out='┴', bottom_out='┬')
--- ascii_tree/draw.py
+++ ascii_tree/draw.py
@@ -1,31 +1,36 @@
 '''
 Functions for drawing
 '''
+from . import charsets
 from typing import List
 from .params import PADDING
 from .custom_types import Node, Offset
 
 
-def draw_node(screen: list, text: str, left_bound: int, top_bound: int, box_width: int, line_width: int, padding: int=PADDING):
+def draw_node(screen: list, text: str, left_bound: int, top_bound: int, box_width: int,
+              line_width: int, padding: int=PADDING, charset=charsets.Ascii):
     '''
     Update screen buffer with node ascii repr
     '''
-    row_idx = top_bound
     # inclusive left and right boundaries
     right_bound = left_bound + box_width
 
     # top border
-    for i in range(left_bound, right_bound+1):
-        screen[row_idx][i] = '-'
+    for i in range(left_bound+1, right_bound):
+        screen[top_bound][i] = charset.top
+    # top-left corner
+    screen[top_bound][left_bound] = charset.top_left
+    # top-right corner
+    screen[top_bound][right_bound] = charset.top_right
 
     # top padding
-    row_idx += 1
+    row_idx = top_bound + 1
     for p in range(padding):
         for i in range(left_bound, right_bound+1):
             if i == left_bound:
-                screen[row_idx + p][i] = '|'
+                screen[row_idx + p][i] = charset.left
             elif i == right_bound:
-                screen[row_idx + p][i] = '|'
+                screen[row_idx + p][i] = charset.right
 
     # create body of box
     # each row will contain box_width total characters
@@ -34,7 +39,7 @@ def draw_node(screen: list, text: str, left_bound: int, top_bound: int, box_widt
     idx = 0  # iterates over chars in text
     while idx < len(text):
         # left boundary
-        screen[row_idx][left_bound] = '|'
+        screen[row_idx][left_bound] = charset.left
         # left padding
         for i in range(padding):
             screen[row_idx][left_bound + i + 1] = ' '
@@ -46,7 +51,7 @@ def draw_node(screen: list, text: str, left_bound: int, top_bound: int, box_widt
         for i in range(padding):
             screen[row_idx][right_bound - i - 1] = ' '
         # right boundary
-        screen[row_idx][right_bound] = '|'
+        screen[row_idx][right_bound] = charset.right
         idx += line_width
         row_idx += 1
 
@@ -54,17 +59,23 @@ def draw_node(screen: list, text: str, left_bound: int, top_bound: int, box_widt
     for p in range(padding):
         for i in range(left_bound, right_bound+1):
             if i == left_bound:
-                screen[row_idx + p][i] = '|'
+                screen[row_idx + p][i] = charset.left
             elif i == right_bound:
-                screen[row_idx + p][i] = '|'
+                screen[row_idx + p][i] = charset.right
 
     row_idx += padding
+
+    bottom_bound = row_idx
+    # bottom-left corner
+    screen[bottom_bound][left_bound] = charset.bottom_left
+    # bottom-right corner
+    screen[bottom_bound][right_bound] = charset.bottom_right
     # bottom border
-    for i in range(left_bound, right_bound +1):
-        screen[row_idx][i] = '-'
+    for i in range(left_bound+1, right_bound):
+        screen[bottom_bound][i] = charset.bottom
 
 
-def draw_edge(screen, src: Node, dest: Node):
+def draw_edge(screen, src: Node, dest: Node, charset=charsets.Ascii):
     '''
     draw an edge between src and destination nodes.
     an edge always extends from the side/bottom of src node and goes to the
@@ -86,37 +97,56 @@ def draw_edge(screen, src: Node, dest: Node):
     if src_lbound > dest_x:
         # edge will go from left of source box
         # y-axis of where to extend from source
-        # first draw horizontal leg
+        # draw horizontal leg
         for i in range(src_lbound, dest_x-1, -1):
-            screen[src_ymiddle][i] = '-'
-        # then draw vertical leg
+            screen[src_ymiddle][i] = charset.xside
+        # draw left out going portrusion
+        screen[src_ymiddle][src_lbound] = charset.left_out
+        # draw elbow of edge
+        screen[src_ymiddle][dest_x] = charset.top_left
+        # draw vertical leg
         for i in range(src_ymiddle+1, dest_y):
-            screen[i][dest_x] = '|'
+            screen[i][dest_x] = charset.yside
     elif src_rbound < dest_x:
         # edge will go from right of source box
         for i in range(src_rbound, dest_x+1):
-            screen[src_ymiddle][i] = '-'
+            screen[src_ymiddle][i] = charset.xside
+        # right protrusion
+        screen[src_ymiddle][src_rbound] = charset.right_out
+        # elbow
+        screen[src_ymiddle][dest_x] = charset.top_right
         # then draw vertical leg
         for i in range(src_ymiddle+1, dest_y):
-            screen[i][dest_x] = '|'
+            screen[i][dest_x] = charset.yside
     else:
         # edge will go from underneath source box
         src_ybottom = ascii_src.box_height + ascii_src.position.top
         for i in range(src_ybottom, dest_y):
-            screen[i][dest_x] = '|'
+            screen[i][dest_x] = charset.yside
+        # bottom protrusion - goes on box which is one unit
+        # higher hence -1
+        screen[src_ybottom-1][dest_x] = charset.bottom_out
+
+    # every dest box will have a top out
+    screen[dest_y][dest_x] = charset.top_out
 
 
-def draw(screen: List[List[str]], root: Node, padding: int=PADDING):
+def draw(screen: List[List[str]], root: Node, padding: int=PADDING, charset=charsets.Unicode):
     '''
     traverse tree and draw nodes and edges
     '''
     stack = [root]
+    edges = []
     while stack:
         node = stack.pop()
         left_bound, top_bound = node.box.position
         box_width, line_width = node.box.box_width, node.box.line_width
-        draw_node(screen, node.val, left_bound, top_bound, box_width, line_width, padding)
+        draw_node(screen, node.val, left_bound, top_bound, box_width, line_width, padding, charset)
         for child in node.children:
             # order doesn't matter since each node is drawn independently
             stack.append(child)
-            draw_edge(screen, node, child)
+            edges.append((node, child))
+    # draw edges at the end since draw_edge handles
+    # adding outgoing protrusion chars on box
+    for node, child in edges:
+        draw_edge(screen, node, child, charset)
